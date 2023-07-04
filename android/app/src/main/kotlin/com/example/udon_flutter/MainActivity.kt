1061f0440447a02681a20d158b1713da3682e118
ljyo2o9 <yggosxoljy@gmail.com>
2023-07-04 11:12:57 +0900

    파일 추가와 HomePage생성

---
+++ android/app/src/main/kotlin/com/example/udon_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.udon_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:udon_flutter/screen/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const `Size(360, 690),
+      builder: (context, child) {
+        return const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: HomePage(),
+        );
+      },
+    );
+  }
+}
